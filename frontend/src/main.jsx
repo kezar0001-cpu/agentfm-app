9307b9a393ba5c97016364252485b4d94ab0a3a5
kezar0001-cpu <kezar0001@gmail.com>
2025-10-03 12:46:27 +0000

    Revert demo sign-in additions

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import App from './App.jsx';
-import { AuthProvider } from './hooks/useAuth';
 import './i18n.js';
 
 const queryClient = new QueryClient({
@@ -46,9 +45,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <BrowserRouter>
-          <AuthProvider>
-            <App />
-          </AuthProvider>
+          <App />
         </BrowserRouter>
       </ThemeProvider>
     </QueryClientProvider>
