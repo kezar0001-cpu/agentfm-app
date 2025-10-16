7013683a7b4e762d455da46d783dad9cad3f9f4a
KezarAlbaldawi <kezar0001@gmail.comm>
2025-10-16 14:46:10 +1100

    step 3

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -45,12 +45,11 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
+     <ThemeProvider theme={theme}>
++        <BrowserRouter>
++          <App />
++        </BrowserRouter>
++      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
