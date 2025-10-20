9ce605e9467bdfe64de33dfee4b3b9e87df45d10
KezarAlbaldawi <kezar0001@gmail.comm>
2025-10-20 12:40:54 +1100

    insp

--- backend/prisma/migrations/20250204120000_international_address_fields/migration.sql
+++
@@ -1,5 +0,0 @@
--- Adjust property address fields for international support
-ALTER TABLE "Property"
-  ALTER COLUMN "state" DROP NOT NULL,
-  ALTER COLUMN "zipCode" DROP NOT NULL,
-  ALTER COLUMN "country" DROP DEFAULT;
